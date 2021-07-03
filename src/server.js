b9682509c39251c4b4dbdd52fb3d49d73ef6ec25
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-03 16:58:04 +0800

    refactor: Remove unused variable

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const path = require("path");
 const fs = require("fs");
 
-const { port, host } = require("./config");
+const { port } = require("./config");
 
 const staticPath = path.join("./", "static");
 const apiPath = path.join("./", "api");
