6789ad60718710a98c333fc481d0dcbe0bfb102c
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-03 16:07:21 +0800

    feat: Added fallback port

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = {
-  port: process.env.EXPRESS_APP_PORT,
+  port: process.env.EXPRESS_APP_PORT | 8888,
   host: process.env.EXPRESS_APP_HOST,
   url: process.env.EXPRESS_APP_HOST_URL,
 };
