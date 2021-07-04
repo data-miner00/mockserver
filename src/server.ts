cd41ab2151a4fb5f0725b4172b4e1587d7dd5c6f
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-04 19:51:19 +0800

    feat: Added Deno server

---
+++ src/server.ts
@@ -0,0 +1,44 @@
+import { Application } from "https://deno.land/x/abc@v1.3.3/mod.ts";
+import * as path from "https://deno.land/std@0.100.0/path/mod.ts";
+import { walkSync } from "https://deno.land/std@0.100.0/fs/mod.ts";
+
+import { port } from "./config.ts";
+
+const app: Application = new Application();
+
+const staticPath: string = path.join("./", "static");
+const apiPath: string = path.join("./", "api");
+
+type Entry = {
+  path: string;
+  name: string;
+  isFile: boolean;
+  isDirectory: boolean;
+  isSymlink: boolean;
+};
+
+const entries: Entry[] = [];
+
+for (const entry of walkSync(apiPath)) {
+  entries.push(entry);
+}
+
+const filenames: string[][] = entries
+  .filter((entry: Entry): boolean => !entry.isDirectory)
+  .map((entry: Entry): string => entry.name)
+  .map((filename: string) => filename.split("."));
+
+console.log(filenames);
+
+filenames.forEach((filename) => {
+  const fileData: any = JSON.parse(
+    Deno.readTextFileSync(`./api/${filename[0]}.${filename[1]}`)
+  );
+  console.log(fileData);
+  app.get(`/${filename[0]}`, (context) => fileData);
+});
+
+app
+  .static("/", staticPath)
+  // .file("/", path.join(staticPath, "index.html"))
+  .start({ port });
